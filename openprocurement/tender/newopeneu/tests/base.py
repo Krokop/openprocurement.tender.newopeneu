67e35494f4722ccd2232d9016dd8fea5d963c806
Ira <iryna.pomazan@unity-bars.com>
2017-02-06 15:17:51 +0200

    Add views/questions and tests/questions

--- openprocurement/tender/newopeneu/tests/base.py
+++ openprocurement/tender/newopeneu/tests/base.py
@@ -2,6 +2,7 @@ from copy import deepcopy
 from datetime import datetime, timedelta
 
 from openprocurement.tender.openeu.tests.base import test_tender_data as base_test_tender_data
+from openprocurement.tender.newopeneu.constants import TENDERING_DAYS
 
 now = datetime.now()
 test_tender_data = deepcopy(base_test_tender_data)
